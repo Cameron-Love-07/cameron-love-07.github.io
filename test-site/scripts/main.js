abc9f7ff76d972908bd47a660db16edfe7d99d32
Cameron-Love-07 <158083792+Cameron-Love-07@users.noreply.github.com>
2024-02-08 13:50:08 -0500

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,14 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+const bus = 6 + 9 + 16;
+console.log(bus)
+
+function multiply(num1, num2) {
+  let result = num1 * num2;
+  return result;
+}
+
+multiply(4, 7);
+multiply(20, 20);
+multiply(0.5, 3);
